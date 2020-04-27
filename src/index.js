de684954b19f2a9d099be4343965ea589ed375d4
tuongdang0406 <tuongdang0406@gmail.com>
2020-04-27 16:39:41 +0700

    added redux on request API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunkMiddleware from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
 //import * as serviceWorker from './serviceWorker';
 
-const store = createStore(searchRobots);
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
